a9a9d58441697cf5cd80aab28dfe32c4ae4c4f0f
kalaibca2001-blip <kalaibca2001@gmail.com>
2026-07-03 11:35:35 +0530

    Initial Commit

---
+++ ExpenditureApp/ExpenditureApp/ExpenditureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenditureAppApp.swift
+//  ExpenditureApp
+//
+//  Created by VC on 03/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenditureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
